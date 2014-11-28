7b52a4fb21c841347ae8f023fe8b9d5ff31a767b
Phil Bolduc <phil.bolduc@albertahealthservices.ca>
2014-11-28 09:58:14 -0800

    Added the provision step

--- packer/builder/hyperv/iso/builder.go
+++ packer/builder/hyperv/iso/builder.go
@@ -259,6 +259,7 @@ func (b *Builder) Run(ui packer.Ui, hook packer.Hook, cache packer.Cache) (packe
 
 		//new(StepUpdateIntegrationServices),
 
+		&common.StepProvision{},
 		//new(StepSysprep),
 
 		new(hypervcommon.StepUnmountFloppyDrive),
