05b123a70d3a5194160baaa5fff2d9a835b53a68
Phil Bolduc <phil.bolduc@albertahealthservices.ca>
2015-01-04 11:04:59 -0800

    Examples are at https://github.com/pbolduc/windows-vm

--- bin/config-file-example/probe.ps1
+++
@@ -1,9 +0,0 @@
-Write-Host "Starting file script..." 
-
-Write-Host 'Executing [DateTime]::Now...' 
-[DateTime]::Now
-
-Write-Host 'Executing Install-WindowsFeature -Name "XPS-Viewer" -IncludeAllSubFeature' 
-Install-WindowsFeature -Name "XPS-Viewer" -IncludeAllSubFeature
-
-Write-Host "File script finished!" 
